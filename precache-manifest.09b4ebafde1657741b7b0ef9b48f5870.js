a3f3d515a3f9ae1bf67e12167bc67f4d717ea5ca
tuananh.le <tuananh.le@foodycorp.vn>
2019-07-25 16:42:14 +0700

    Updates

---
+++ precache-manifest.09b4ebafde1657741b7b0ef9b48f5870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a3a79dec5d5b7fbcaf53299c79dc56",
+    "url": "/lucky-draw/index.html"
+  },
+  {
+    "revision": "0a3e578cd1d67c807593",
+    "url": "/lucky-draw/static/css/2.88bb5262.chunk.css"
+  },
+  {
+    "revision": "2c5eddeb99611bd791c6",
+    "url": "/lucky-draw/static/css/main.56c01886.chunk.css"
+  },
+  {
+    "revision": "0a3e578cd1d67c807593",
+    "url": "/lucky-draw/static/js/2.f69e3185.chunk.js"
+  },
+  {
+    "revision": "2c5eddeb99611bd791c6",
+    "url": "/lucky-draw/static/js/main.f6e4eeda.chunk.js"
+  },
+  {
+    "revision": "51c2cf2980d917b98984",
+    "url": "/lucky-draw/static/js/runtime~main.cb9ab0a0.js"
+  }
+]);
